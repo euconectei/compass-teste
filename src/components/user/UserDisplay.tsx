0604c656b9bdebe70d485765766576e5382604ce
Rafael França <rafaelfms@gmail.com>
2022-03-13 21:07:03 -0300

    implemented UserDisplay compact option

--- src/components/user/UserDisplay.tsx
+++ src/components/user/UserDisplay.tsx
@@ -4,9 +4,10 @@ import UserNotFound from './UserNotFound';
 type UserDisplayComponentType = {
   user: UserType;
   username: string;
+  compact?: boolean;
 };
 
-const UserDisplay = ({ user, username }: UserDisplayComponentType) => {
+const UserDisplay = ({ user, username, compact }: UserDisplayComponentType) => {
   if (Object.keys(user).length === 0) {
     return <UserNotFound username={username} />;
   }
@@ -22,34 +23,52 @@ const UserDisplay = ({ user, username }: UserDisplayComponentType) => {
           />
           <div className='card-body'>
             <h5 className='card-title'>{user.login}</h5>
-            <ul className='list-group list-group-flush'>
-              <li className='list-group-item'>
-                <span className='font-weight-bold'>Nome:</span> {user.name}
-              </li>
-              <li className='list-group-item'>
-                <span className='font-weight-bold'>E-mail:</span> {user.email}
-              </li>
-              <li className='list-group-item'>
-                <span className='font-weight-bold'>Repositórios:</span>{' '}
-                {user.public_repos}
-              </li>
-              <li className='list-group-item'>
-                <span className='font-weight-bold'>Seguidores:</span>{' '}
-                {user.followers}
-              </li>
-              <li className='list-group-item'>
-                <span className='font-weight-bold'>Seguindo:</span>{' '}
-                {user.following}
-              </li>
-            </ul>
+            {!compact && (
+              <ul className='list-group list-group-flush'>
+                <li className='list-group-item'>
+                  <span className='font-weight-bold'>Nome:</span> {user.name}
+                </li>
+                <li className='list-group-item'>
+                  <span className='font-weight-bold'>E-mail:</span> {user.email}
+                </li>
+                <li className='list-group-item'>
+                  <span className='font-weight-bold'>Repositórios:</span>{' '}
+                  {user.public_repos}
+                </li>
+                <li className='list-group-item'>
+                  <span className='font-weight-bold'>Seguidores:</span>{' '}
+                  {user.followers}
+                </li>
+                <li className='list-group-item'>
+                  <span className='font-weight-bold'>Seguindo:</span>{' '}
+                  {user.following}
+                </li>
+              </ul>
+            )}
             <hr className='divider' />
-            <div className='d-flex justify-content-between'>
-              <a href={`/${user.login}/repos`} className='btn btn-primary'>
-                Repos
-              </a>
-              <a href={`/${user.login}/starred`} className='btn btn-primary'>
-                Starred
-              </a>
+            <div className='d-grid gap-2'>
+              {compact && (
+                <a
+                  href={`/${user.login}`}
+                  className='btn btn-primary btn-block mb-3'
+                >
+                  Ver detalhes
+                </a>
+              )}
+              <div className='d-flex justify-content-between'>
+                <a
+                  href={`/${user.login}/repos`}
+                  className='btn btn-outline-primary'
+                >
+                  Repos
+                </a>
+                <a
+                  href={`/${user.login}/starred`}
+                  className='btn btn-outline-primary'
+                >
+                  Starred
+                </a>
+              </div>
             </div>
           </div>
         </div>
