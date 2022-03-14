715ce44c1a34e7a63b46ca45676afde23b20e0e1
Rafael França <rafaelfms@gmail.com>
2022-03-13 21:10:18 -0300

    implemented loading

--- src/pages/HomeSignedOut.tsx
+++ src/pages/HomeSignedOut.tsx
@@ -1,8 +1,31 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { FiGithub } from 'react-icons/fi';
+import { useLoading } from '../contexts/LoadingContext';
+import AuthService from '../services/AuthService';
 
 const HomeSignedOut = () => {
-  const [data, setData] = useState({});
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get('code');
+  const { setLoading } = useLoading();
+
+  const getAccessToken = async () => {
+    try {
+      if (code) {
+        setLoading(true, 'Solicitando token de acesso...');
+        const res = await AuthService.getAccessToken(code);
+        const { access_token } = res.data;
+        localStorage.setItem('access_token', access_token);
+      }
+    } catch (error) {
+      console.log({ error });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAccessToken();
+  }, []);
 
   const { REACT_APP_CLIENT_ID } = process.env;
   const redirect_uri = 'http://localhost:3000';
@@ -12,9 +35,6 @@ const HomeSignedOut = () => {
       <a
         className='login-link'
         href={`https://github.com/login/oauth/authorize?scope=user&client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${redirect_uri}`}
-        onClick={() => {
-          setData({ ...data, errorMessage: '' });
-        }}
       >
         <div>
           <FiGithub size='3rem' />
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { UserDisplay, UserNotFound } from '../components/user';
+import { useLoading } from '../contexts/LoadingContext';
 import GithubService from '../services/GithubService';
 import { UserType } from '../types/UserType';
 
 const UserPage = () => {
   const [userData, setUserData] = useState<UserType>({} as UserType);
   const { username } = useParams<'username'>() as { username: string };
+  const { setLoading } = useLoading();
 
   const getData = async () => {
+    setLoading(true, 'Carregando dados do usuário...');
     if (username) {
       setUserData(await GithubService.getUser(username));
     }
+    setLoading(false);
   };
 
   useEffect(() => {
